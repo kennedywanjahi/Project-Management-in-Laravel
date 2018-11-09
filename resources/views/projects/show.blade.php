d4045b5e42e5da5928db105102edb6e209cc3f4c
ken <kennedywanjahi@gmail.com>
2018-11-09 11:14:20 +0300

    commenting ui

--- resources/views/projects/show.blade.php
+++ resources/views/projects/show.blade.php
@@ -76,6 +76,56 @@
                 </div>
 
             @endforeach
+{{--  --}}
+<div class="row">
+		<div class="col-md-12 col-sm-12 col-xs-12 col-lg-12">
+
+            <!-- Fluid width widget -->
+    	    <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">
+                        <span class="glyphicon glyphicon-comment"></span> 
+                        Recent Comments
+                    </h3>
+                </div>
+                <div class="panel-body">
+                    <ul class="media-list">
+
+
+                      @foreach ($project->comments as $comment)
+                        <li class="media">
+                            <div class="media-left">
+                                <img src="http://placehold.it/60x60" class="img-circle">
+                            </div>
+                            <div class="media-body">
+                                <h4 class="media-heading">
+                                    Mauris Eu
+                                    <br>
+                                    <small>
+                                        commented on <a href="#">Post Title</a>
+                                    </small>
+                                </h4>
+                                <p>
+                                    {{$comment->body}}
+                                </p>
+                                Proof
+                                <p>
+                                    {{$comment->url}}
+                                </p>
+                            </div>
+                        </li>
+
+                      @endforeach
+
+                    </ul>
+                </div>
+
+
+{{--  --}}
+
+
+
+
           </div>
         </div>
 
